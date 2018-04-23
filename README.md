b6028bc1d3bf99ae808bfdefe8cf3f18b7163baa
raklein <raklein22@gmail.com>
2018-04-23 11:18:35 +0200

    Updated readme and added more instructions to the rscript

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
-# co-relab_source
-Source files for the corelab.io website (also co-relab.github.io).
-The corelab.io webpage is a GitHub Pages webpage, using the custom domain of corelab.io
+# Repository for corelab.io website
+The corelab.io webpage is a GitHub Pages webpage (co-relab.github.io), using the custom domain of corelab.io. It is built for the CORE Lab at the Universite Grenoble Alpes.
 
-This repo contains the code to generate the site. The actual site itself is in the repo "co-relab.github.io".
-This is because GitHub Pages can only have the files for the site to render. So, we build the site in this repo,
-then push the "finished" files to that repo.
+This repo contains the code to generate the site. It is built in the [Blogdown R Package](https://bookdown.org/yihui/blogdown/). The actual site itself is in the [co-relab.github.io](https://github.com/co-relab/co-relab.github.io) repo.
+This is because GitHub Pages can only contain the "published" files for the site to render. So, we build the site in this repo,
+then "publish" the finished files to that repo.
 
-More instructions to follow later.
+Basic steps to install/use/make changes:  
+1. [Install Git](https://help.github.com/articles/set-up-git/) (if necessary -- comes on Mac by default)  
+2. [Clone](https://help.github.com/articles/cloning-a-repository/) (e.g., download the files and create a Git repository on your local hard disk) these two repos locally. Open a [Terminal](http://blog.teamtreehouse.com/introduction-to-the-mac-os-x-command-line) (or [cmd prompt](https://www.howtogeek.com/235101/10-ways-to-open-the-command-prompt-in-windows-10/)), navigate to where you want the files to be stored (use the 'cd' command in Terminal), and run these two commands:  
+`"git clone https://github.com/co-relab/co-relab_source.git"`
+`"git clone https://github.com/co-relab/co-relab.github.io.git"`
 
-Basic steps to use:
-1. Install Git (if necessary)
-2. Clone these two repos locally. Open a Terminal (or cmd prompt), navigate to where you want the files to be stored, and run these two commands:
-"git clone https://github.com/co-relab/co-relab_source.git" (source files repo)
-"git clone https://github.com/co-relab/co-relab.github.io.git" (github pages repo)
+This will create two new subdirectories/Git repos: co-relab_source and co-relab.github.io
 
-Then, navigate on your computer to the source files repo, and open the .rproj file (and then the r script file).
-Using R Projects eliminates the need to set a working directory each time (it's automatically set to the folder containing the .rproj file)
+You will also need R and R Studio installed.
+
+Then, navigate on your computer to the source files repo, and open the .rproj file (co-relab_source.rproj) in R Studio. Then open the r script (co-relab_source_rscript.R). Using R Projects eliminates the need to set a working directory each time (it's automatically set to the folder containing the .rproj file)
+
+Then, follow the instructions in the R script for more instructions.
--- co-relab_source_rscript.R
+++ co-relab_source_rscript.R
@@ -5,55 +5,56 @@
 # A second is created to store the raw files/blogdown files because the first repo can only store the "ready-for-web" files.
 # or it won't render
 
-# To start, clone both GitHub repos locally: open a terminal, use "cd" to navigate to the directory you want
-# to download to (it will make a new subdirectory below your current directory for each repo)
-# Then run in the terminal the URL to the repos
+# To start, if you haven't already, clone both GitHub repos locally: open a terminal, use "cd" to 
+# navigate to the directory you want to download to (it will make a new subdirectory below your current
+# directory for each repo). Then run in the terminal the URL to the repos
 # e.g. "git clone https://github.com/co-relab/co-relab_source.git" (source files repo)
 # and "git clone https://github.com/co-relab/co-relab.github.io.git" (github pages repo)
-# Note: you need git installed on your computer or this won't work (comes with MacOS but not PC)
+# Note: you need git installed on your computer or this won't work (comes with MacOS, needs installed on Windows)
 
 # Many lines below commented out because they are only run once to download files, which are
 # then manually edited. 
-#### WARNING: Uncommenting and running from the start will overwrite everything to defaults
+#### WARNING: Uncommenting and running from the start will overwrite everything to defaults.
+#### So, pick and choose the lines you need. Mostly this file is just a tutorial.
 
 # We use an .rproj file so we can skip setwd()
 
-# Load blogdown package and install Hugo
+# Load blogdown package and install Hugo (if needed)
 library(blogdown)
 #install_hugo()
 
 # At site creation, I ran the below lines to generate the site in Blogdown.
-# Running this again would overwrite the site with a new blank site. Left as an example.
+# Running this again would overwrite the site with a new blank site. I leave the code as an example.
 
 # Can use different themes (https://themes.gohugo.io/) by specifying the github repo theme=''
-# The URL comes from the end of the theme's GitHub account and repo.
-# Here, using the theme https://github.com/gcushen/hugo-academic
-# For this step the directory needs to be completely empty (I just temporarily move the existing files out and back in)
-# Trying Elate theme
+# The URL comes from the theme's GitHub account and repo. Here, using the theme https://github.com/saey55/hugo-elate-theme
+# For this step, the R Project directory (e.g., working directory) needs to be completely empty 
+# (I just temporarily move the existing files out and back in manually)
 #new_site(theme='saey55/hugo-elate-theme')
 
 # Now, there are many files in the directory. As far as I know, these must be edited manually to add/change content.
-# The main file for this theme is config.toml
+# The main file for this theme is config.toml, which is where you will make most of your changes.
 # Most images are under /themes/static/images
 
 # This theme comes with 4 different CSS styles. I couldn't find a setting to change them, so to use style4 I manually renamed:
 # themes/hugo-elate-theme/static/css/style4.css to style.css
 # themes/hugo-elate-theme/static/css/style4.css.map to style.css.map
 
-# Here's an example to create a new post, if this is a blog.
+# Here's an example to create a new post, if this is a blog. I don't think this has a function with Elate.
 # new_post(title = 'hello-world.Rmd', ext = ".Rmd")
 
-# Serve site to generate a preview of the webpage.
+# The serve site function generates a preview of the webpage.
 # Click the button next to the broom in the viewer to open in a browser.
 serve_site()
 
 # To deploy to web:
 # Below command by default renders the site into a new subfolder in the directory called "public".
 # At the top of config.toml you can specify a different directory with: publishDir = ""
-# In this case I added publishDir = "../co-relab.github.io" which generates these files in the github pages repo folder.
+# In this case I added publishDir = "../co-relab.github.io" which generates these files in the github pages repo folder
+# as long as you downloaded the repos to folders under the same directory (the '..' command moves up one directory level)
 build_site()
 
-# To commit to github pages, open the terminal in Rstudio (should be next to Console)
+# To commit to github pages, open the terminal (or use the "Terminal" window in R Studio -- NOT the console)
 # Navigate to the local repo you want to commit with "cd"
 # Type the three commands:
 #git add --all
